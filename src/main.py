e22e9c0c503b9ff0984b91a654cc0397da21de6e
Felix Russo <felix.russo@epfl.ch>
2020-12-03 18:03:38 +0100

    have fun karel :)

--- src/main.py
+++ src/main.py
@@ -26,6 +26,7 @@ def train(model, x_train, y_train, x_test, y_test, loss_func, optimizer, epochs:
             y_train_batch = y_train[batch_i]
 
             predictions = model.forward(x_train_batch)
+            
             loss = loss_func(predictions, y_train_batch)
 
             epoch_loss += loss.item() / batch_count
@@ -47,40 +48,66 @@ def train(model, x_train, y_train, x_test, y_test, loss_func, optimizer, epochs:
 
 class convolutional_nn(torch.nn.Module):
 
-    def __init__(self, n_features, n_filters = 10):
+    def __init__(self, n_features, n_convols = 3, n_filters=None, n_filters_const = 10):
         super().__init__()
         
-        self.n_filters = n_filters
-        self.conv2 = torch.nn.Conv1d(n_features, n_filters, kernel_size=2)
-        self.conv3 = torch.nn.Conv1d(n_features, n_filters, kernel_size=3)
-        self.conv4 = torch.nn.Conv1d(n_features, n_filters, kernel_size=4)
-        self.conv5 = torch.nn.Conv1d(n_features, n_filters, kernel_size=5)
-        
-        temp_size = n_filters
-        self.linear1 = torch.nn.Linear(4*n_filters, temp_size)
+        if n_filters is None:
+            self.n_filters = np.ones(n_convols, dtype='int') * n_filters_const
+        else:
+            self.n_filters = n_filters
+            
+        self.n_convols = n_convols
+        self.convs = torch.nn.ModuleList()
+        for i in range(self.n_convols):
+            self.convs.append(torch.nn.Conv1d(n_features, self.n_filters[i], kernel_size=i+2))
+ 
+        temp_size = 10
+        self.linear1 = torch.nn.Linear(np.sum(self.n_filters), temp_size)
         self.linear2 = torch.nn.Linear(temp_size, 2)
-        
+        """
+        self.n_filters = n_filters
+        self.conv2 = torch.nn.Conv1d(n_features, n_filters_const, kernel_size=2)
+        self.conv3 = torch.nn.Conv1d(n_features, n_filters_const, kernel_size=3)
+        self.conv4 = torch.nn.Conv1d(n_features, n_filters_const, kernel_size=4)
+        self.conv5 = torch.nn.Conv1d(n_features, n_filters_const, kernel_size=5)
+
+        temp_size = n_filters_const
+        self.linear1 = torch.nn.Linear(4*n_filters_const, temp_size)
+        self.linear2 = torch.nn.Linear(temp_size, 2)
+        """
     def forward(self, x):
     
         relu = torch.nn.functional.relu
-        
         #apply filters and take maximum element
-        x2 = torch.max(self.conv2(x), dim=2)
-        x3 = torch.max(self.conv3(x), dim=2)
-        x4 = torch.max(self.conv4(x), dim=2)
-        x5 = torch.max(self.conv5(x), dim=2)
+        max_vals = []
+        print("x=", x.shape)
+        for i in range(self.n_convols):
+            conv = self.convs[i](x)
+            print("conv=", conv.shape)
+            neg_inf_tensor = torch.zeros(conv.shape)
+            neg_inf_tensor[:] = float('-inf')
+            max_vals.append(torch.max(torch.where(torch.isnan(conv), neg_inf_tensor, conv), dim=2).values)
+            #max_vals.append(torch.max(conv, dim=2).values)
+            #print(torch.any(torch.eq(torch.max(torch.where(torch.isnan(conv), neg_inf_tensor, conv), dim=2), float('-inf'))))
+            #print(torch.any(torch.isnan(torch.where(torch.isnan(conv), neg_inf_tensor, conv))))
         
         
         #concatenate
-        x = torch.cat((x2.values, x3.values, x4.values, x5.values), dim=1) 
-        
+        x = torch.cat(max_vals, dim=1) 
+
         #regularize
         #x = torch.nn.functional.dropout(x, training=self.training)
         
+        """     
+        x2 = torch.max(self.convs[0](x), dim=2)
+        x3 = torch.max(self.convs[1](x), dim=2)
+        x4 = torch.max(self.convs[2](x), dim=2)
+        x5 = torch.max(self.convs[3](x), dim=2)
+        x = torch.cat((x2.values, x3.values, x4.values, x5.values), dim=1)
+        """
         #do linear trafo + relu
         x = relu(self.linear1(x))
         x = self.linear2(x)
-        
         return x
 
 class LogisticRegressionModel(torch.nn.Module):
@@ -138,6 +165,8 @@ def construct_sequential_tensors(emb: Embedding, tweets: Tweets, tweet_count: in
     # todo maybe store index in x instead of expanded embedding
     x = torch.zeros(2 * tweet_count, max_length, emb.size)
     y = torch.empty(2 * tweet_count, dtype=torch.long)
+    #convert zeros to nan
+    x[:] = float('NaN')
     cropped_count = 0
 
     tweet_i = 0
@@ -147,7 +176,8 @@ def construct_sequential_tensors(emb: Embedding, tweets: Tweets, tweet_count: in
             tokens = tweet_as_tokens(tweet, emb.word_dict)
             if len(tokens) > max_length:
                 cropped_count += 1
-
+            if len(tokens) < 4:
+                continue
             for token_i, token in enumerate(tokens):
                 if token_i >= max_length:
                     break
@@ -195,17 +225,19 @@ def main():
     print(f"Using device {device}")
 
     print("Constructing tensors")
-    x, y = construct_sequential_tensors(emb=emb, tweets=tweets, tweet_count=10000, max_length=n_channels)
+    x, y = construct_sequential_tensors(emb=emb, tweets=tweets, tweet_count=100, max_length=n_channels)
     #x, y = construct_mean_tensors(emb=emb, tweets=tweets, tweet_count=1000)
-
+    
+    x = x.permute(0,2,1)
     x = x.to(device)
     y = y.to(device)
 
     x_train, y_train, x_test, y_test = split_data(x, y, train_ratio)
     
-    x = x.permute(0,2,1)
+    
     loss_func = torch.nn.CrossEntropyLoss() 
-    model = convolutional_nn(n_features=n_features, n_filters=10)
+    model = convolutional_nn(n_features=n_features, n_convols=4, n_filters_const=10)
+    model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     print("Training...")
     losses, train_accs, test_accs = train(model, x_train, y_train, x_test, y_test, 
@@ -216,7 +248,6 @@ def main():
     pyplot.plot(test_accs, label="test acc")
     pyplot.legend()
     pyplot.show()
-
 
 if __name__ == '__main__':
     main()
