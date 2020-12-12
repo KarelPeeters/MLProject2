c80a9a01f9d46b87e6f030a618516efbd9e68a93
Karel Peeters <karel.peeters.leuven@gmail.com>
2020-12-13 00:40:58 +0100

    Add examples to embedding experiment.

--- src/experiment.py
+++ src/experiment.py
@@ -3,7 +3,7 @@ from embedding import load_embedding
 emb = load_embedding(10_000, 0, 200)
 
 print()
-BASE_WORDS = ["math", "madrid", "5", "ball", "happy", "sad"]
+BASE_WORDS = ["math", "madrid", "5", "ball", "happy", "sad", "false", "favorite"]
 for word in BASE_WORDS:
     similar_words = emb.find(emb.embed(word), 6)[1:]
     print(f"{word} & {', '.join(similar_words)} \\\\")
